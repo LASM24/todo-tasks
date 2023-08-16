f942f3b07e2adaa04cbce37116599d04c632c91b
LASM24 <luisalbertosanabriamendez@gmail.com>
2023-08-15 19:28:31 -0500

    variables de entorno, modelo, api, crear tarea

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const api = require('./api');
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
@@ -9,3 +13,5 @@ app.listen(port, function () {
 app.get('/', function (req, res) {
     res.send("By Luis Sanabria");
 });
+
+app.use('/api', api);
